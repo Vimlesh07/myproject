bccc203f435399d0d00142a6ca45f79e6fd079b0
Vimlesh Nishad <Vimlesh Nishad@DESKTOP-JLORLVM>
2019-12-05 22:53:59 +0530

    log4j2 and swagger configurations

--- gs-accessing-data-mysql-complete/src/main/java/com/sathya/rms/admin/Controller/CityController.java
+++ gs-accessing-data-mysql-complete/src/main/java/com/sathya/rms/admin/Controller/CityController.java
@@ -1,5 +1,7 @@
 package com.sathya.rms.admin.Controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,31 +18,52 @@ import com.sathya.rms.admin.service.CityService;
 @RestController
 @RequestMapping(path="/cities")
 public class CityController {
+	private static final Logger logger = LogManager.getLogger(CityController.class);
 	
 	@Autowired
    private CityService cityService;
 	
-	@GetMapping(path="/getAllCity")
-	public Iterable<City> getAllCity()
-	{
-		return cityService.getAllCity();
+	@GetMapping(path = "/getAllCities")
+	public	Iterable<City>	getAllCity(){
+		logger.info("get All Cities method execution started");
+		Iterable<City> result = null;
+		try {
+			result = cityService.getAllCity();
+			logger.debug("result is {0}",result);
 		}
+		catch(Exception e) {
+			logger.error("exception happens and exception info is {0} ",e);
+		}
+		logger.info("get All Cities method execution completed");
+		
+		return result;
+	}
 	
-	@PostMapping(path="/addCity")
+	@PostMapping(path = "/addCity")
 	public City insertCity(@RequestBody City city) {
-	
-		return cityService.insertCity(city);
+		logger.info("insertCity method execution started");
+		
+		logger.debug("input data is {0}",city);
+		
+		City result= null;
+		try {
+			result=cityService.insertCity(city);
+			logger.debug("result is {0}",result);
+		}
+		catch(Exception e) {
+			logger.error("input data is {0}",city);
+		}
+		logger.info("insertCity method execution completed");
+		return result;
 	}
 	
 	@PutMapping(path="/updateCity")
-	public City  updateCity(@RequestBody City city) {
+	public City updateCity(@RequestBody City city) {
 		return cityService.updateCity(city);
-		
 	}
 	
 	@DeleteMapping(path="/deleteCity/{id}")
 	public void deleteCity(@PathVariable("id") Integer id) {
-		
-		cityService.deleteCity(id);		
+		cityService.deleteCity(id);
 	}
 }
